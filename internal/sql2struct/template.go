9cd0f134c7f3951e47f071453496394b44b7da24
Jsoneft <jasonleft@qq.com>
2021-01-13 23:22:50 +0800

    add sql2struct

---
+++ internal/sql2struct/template.go
@@ -0,0 +1,30 @@
+package sql2struct
+
+const strutTpl = `type {{.TableName | ToCamelCase}} struct {
+{{range .Columns}}	{{ $length := len .Comment}} {{ if gt $length 0 }}// {{.Comment}} {{else}}// {{.Name}} {{ end }}
+	{{ $typeLen := len .Type }} {{ if gt $typeLen 0 }}{{.Name | ToCamelCase}}	{{.Type}}	{{.Tag}}{{ else }}{{.Name}}{{ end }}
+{{end}}}
+
+func (model {{.TableName | ToCamelCase}}) TableName() string {
+	return "{{.TableName}}"
+}`
+
+type StructTemplate struct {
+	strutTpl string
+}
+
+type StructColumn struct {
+	Name    string
+	Type    string
+	Tag     string
+	Comment string
+}
+
+type StructTemplateDB struct {
+	TableName string
+	Columns   []*TableColumn
+}
+
+func NewStructTemplate() *StructTemplate {
+	return &StructTemplate{strutTpl: strutTpl}
+}
